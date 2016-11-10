06aa66574311df2c9e96962ec5c7d883264c09ae
LakshayGoel1 <lakshayg5725@gmail.com>
2016-11-10 16:00:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/Lakshay2.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Lakshay2.jpg'));
+app.get('/ui/Lakshay1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Lakshay1.jpg'));
 });
 
 
