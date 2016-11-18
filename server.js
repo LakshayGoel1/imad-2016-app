f8ae234df4cfec5151f73a53c69b7df47168d314
LakshayGoel1 <lakshayg5725@gmail.com>
2016-11-19 03:32:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,13 +13,34 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/style2.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style2.css'));
+});
+
+app.get('/ui/About2.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'About2.html'));
+});
+
+app.get('/ui/Article.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Article.html'));
+});
+
 app.get('/ui/Lakshay1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Lakshay1.jpg'));
 });
 
-app.get('/ui/Lakshay2.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Lakshay2.jpg'));
+app.get('/ui/instagram.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'instagram.jpg'));
 });
+
+app.get('/ui/twitter.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'twitter.png'));
+});
+
+app.get('/ui/fb.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'fb.png'));
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
